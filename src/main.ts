dbce3a2ad6801aa69ac6382b9bc72c204db3d427
NothingIsLost <nothingislost@fastmail.net>
2021-12-22 11:02:57 -0800

    improve inline/block detection. clear out markdown styling

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { Plugin } from "obsidian";
 import { RangeSetBuilder } from "@codemirror/rangeset";
-import { EditorView, Decoration, DecorationSet, ViewUpdate, ViewPlugin } from "@codemirror/view";
+import { EditorView, Decoration, DecorationSet, ViewUpdate, ViewPlugin, PluginField } from "@codemirror/view";
 import { LanguageSupport } from "@codemirror/language";
 import { javascript } from "@codemirror/lang-javascript";
 import { syntaxHighlight } from "./syntaxHighlight";
@@ -12,69 +12,90 @@ export default class SyntaxHighlightingPlugin extends Plugin {
   }
 
   buildSyntaxViewPlugin() {
+    const plugin = this;
     const viewPlugin = ViewPlugin.fromClass(
       class {
-        decorations: DecorationSet;
+        blockDecorations: DecorationSet;
+        inlineDecorations: DecorationSet;
         language: LanguageSupport;
 
         constructor(view: EditorView) {
           this.language = javascript();
-          this.decorations = this.buildDecorations(view);
+          [this.blockDecorations, this.inlineDecorations] = this.buildDecorations(view);
         }
 
         update(update: ViewUpdate) {
+          console.log("viewUpdate", update);
           if (update.docChanged || update.viewportChanged) {
-            this.decorations = this.buildDecorations(update.view);
+            [this.blockDecorations, this.inlineDecorations] = this.buildDecorations(update.view);
           }
         }
 
-        destroy() {}
-
         parseCodeBlocks(view: EditorView) {
-          const codeBlocks = [];
-          let blockStart: number, blockEnd: number;
+          // This method is currently very inefficient and is mostly just a placeholder
+          // until a better code fence parser is written
+          const codeBlocks: [number, number, string][] = [];
+          let blockStart: number, blockEnd: number, blockType: string;
+          // TODO: iterate only over visible lines in the viewport
           for (let lines = view.state.doc.iterLines(), pos = 0; !lines.next().done; ) {
             let { value } = lines,
               match;
             if ((match = value.match(/<%(\*)?/))) {
               blockStart = pos + match.index;
+              match.index === 0 ? (blockType = "block") : (blockType = "inline");
             }
             if ((match = value.match(/%>/))) {
               blockEnd = pos + match.index + 2;
             }
             pos += value.length + 1;
             if ((blockStart === 0 || blockStart) && (blockEnd === 0 || blockEnd)) {
-              codeBlocks.push([blockStart, blockEnd]);
-              blockStart = blockEnd = null;
+              codeBlocks.push([blockStart, blockEnd, blockType]);
+              blockStart = blockEnd = blockType = null;
             }
           }
           return codeBlocks;
         }
 
         buildDecorations(view: EditorView) {
-          let decorations = new RangeSetBuilder<Decoration>();
+          let tokenDecoSet = new RangeSetBuilder<Decoration>();
+          let lineDecoSet = new RangeSetBuilder<Decoration>();
+          let lineDeco = Decoration.line({
+            class: "HyperMD-codeblock HyperMD-codeblock-bg templater",
+            attributes: { "data-templater-code": "", spellcheck: "false" },
+          });
+          let tokenDeco = Decoration.mark({
+            class: "cm-inline-code templater",
+            attributes: { "data-templater-code": "", spellcheck: "false" },
+          });
+
           const codeBlocks = this.parseCodeBlocks(view);
-          let prevLineMark = -1;
-          for (let [blockStart, blockEnd] of codeBlocks) {
+          for (let [blockStart, blockEnd, blockType] of codeBlocks) {
             const textContent = view.state.doc.sliceString(blockStart, blockEnd);
+
+            let lineStart = view.state.doc.lineAt(blockStart),
+              lineEnd = view.state.doc.lineAt(blockEnd);
+
+            if (blockType == "block") {
+              for (let line = lineStart.number; line <= lineEnd.number; line++) {
+                let curLine = view.state.doc.line(line);
+                lineDecoSet.add(curLine.from, curLine.from, lineDeco);
+              }
+            } else {
+              lineDecoSet.add(blockStart, blockEnd, tokenDeco);
+            }
+
             syntaxHighlight(textContent, this.language, ({ from, to, style }) => {
-              let deco = Decoration.line({
-                class: "HyperMD-codeblock HyperMD-codeblock-bg",
-                attributes: { "data-templater-code": "", spellcheck: "false" },
-              });
-              let line = view.state.doc.lineAt(blockStart + from);
-              style &&
-                prevLineMark !== line.from &&
-                (prevLineMark = line.from) &&
-                decorations.add(line.from, line.from, deco);
-              style && decorations.add(blockStart + from, blockStart + to, Decoration.mark({ class: style }));
+              style && tokenDecoSet.add(blockStart + from, blockStart + to, Decoration.mark({ class: style }));
             });
           }
-          return decorations.finish();
+          return [lineDecoSet.finish(), tokenDecoSet.finish()];
         }
       },
       {
-        decorations: v => v.decorations,
+        provide: [
+          PluginField.decorations.from(viewPlugin => viewPlugin.blockDecorations),
+          PluginField.decorations.from(viewPlugin => viewPlugin.inlineDecorations),
+        ],
       }
     );
     return viewPlugin;
